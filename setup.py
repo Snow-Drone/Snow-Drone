886fe9506577e23ead315dcb173e3a5571051a36
dragonracer9 <vikram.damani@damani.ch>
2025-10-20 12:16:34 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Snow-Drone',
+   version='0.0.1',
+   description='Image acquisition and online processing for drone snowflake data collection.',
+   author='',
+   author_email='',
+   packages=['Snow-Drone'],
+   install_requires=['wheel', 'opencv-python', 'numpy', 'flirpy'],
+)
